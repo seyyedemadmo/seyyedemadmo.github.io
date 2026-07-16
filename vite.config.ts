742e0cf6524c4001662e201f382f681e6ef8ceb4
Emad <emad@My-Old-Friend.home>
2026-07-16 11:44:51 +0330

    fixed base urls

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // Set base to '/<repo-name>/' when deploying to https://<user>.github.io/<repo-name>/
 // Set base to '/' if deploying to a custom domain or a <user>.github.io root repo.
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [vue()],
   build: {
     target: 'esnext',
